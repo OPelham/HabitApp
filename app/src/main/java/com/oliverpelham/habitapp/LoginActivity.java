a42e5eae22fd88bcadbc7899493586df9b609fe1
OPelham <oliverpelham@gmail.com>
2018-10-29 09:44:55 +1300

    Hid actionbar in login container

--- app/src/main/java/com/oliverpelham/habitapp/LoginActivity.java
+++ app/src/main/java/com/oliverpelham/habitapp/LoginActivity.java
@@ -11,6 +11,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+        getSupportActionBar().hide();
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.container, LoginFragment.newInstance())
